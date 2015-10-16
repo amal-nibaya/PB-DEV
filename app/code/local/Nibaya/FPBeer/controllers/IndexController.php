d928070c24819285ba9802982a92196d9db4f80f
amal-nibaya <amal.g@nibaya.com>
2015-10-16 17:37:55 +0530

    Third Commit

--- app/code/local/Nibaya/FPBeer/controllers/IndexController.php
+++ app/code/local/Nibaya/FPBeer/controllers/IndexController.php
@@ -4,7 +4,31 @@ class Nibaya_FPBeer_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         $this->loadLayout(); 
-        $this->getLayout()->getBlock('head')->setTitle($this->__('Welcome'));
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Welcome - Phoenix Beer'));
+        $this->renderLayout();
+    }
+    public function privacyAction()
+    {
+        $this->loadLayout(); 
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Privacy - Phoenix Beer'));
+        $this->renderLayout();
+    }
+    public function licenseAction()
+    {
+         $this->loadLayout(); 
+        $this->getLayout()->getBlock('head')->setTitle($this->__('License - Phoenix Beer'));
+        $this->renderLayout();
+    }
+    public function disclaimerAction()
+    {
+        $this->loadLayout(); 
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Disclaimer - Phoenix Beer'));
+        $this->renderLayout();
+    }
+    public function contactusAction()
+    {
+        $this->loadLayout(); 
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Contact Us - Phoenix Beer'));
         $this->renderLayout();
     }
 }
